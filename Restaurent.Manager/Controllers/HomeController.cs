3c550629789be4165b111235404444554d579955
F88\anhnh13 <anhnh13@f88.vn>
2024-05-07 08:42:23 +0700

    render food, table for home and menu page

--- Restaurent.Manager/Controllers/HomeController.cs
+++ Restaurent.Manager/Controllers/HomeController.cs
@@ -1,12 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Restaurent.Manager.Models;
+using Restaurent.Manager.Models.Datas;
 
 namespace Restaurent.Manager.Controllers
 {
     public class HomeController : Controller
     {
+        AppDbContext context = new AppDbContext();
+
         public IActionResult Index()
         {
-            return View();
+            var tables = context.Table.Include(x => x.Bills).ToList();
+            return View(tables);
         }
 
         public IActionResult Chef()
@@ -14,19 +20,29 @@ namespace Restaurent.Manager.Controllers
             return View();
         }
 
-        public IActionResult Menu()
+        public IActionResult Menu(int id)
         {
-            return View();
+            if(id == 0)
+                return RedirectToAction("Index");
+            var table = context.Table.FirstOrDefault(x => x.Id == id);
+            ViewData["bill"] = new Bill() { Table = table, Records = new List<BillRecord>() };
+            var foods = context.Food.Where(x => x.Status).ToList();
+            return View(foods);
         }
 
         public IActionResult Bill(int id)
         {
-            return View("Menu");
+            ViewData["bill"] = context.Bill.Include(x => x.Records).ThenInclude(x => x.Food).Include(x => x.Table).FirstOrDefault(x => x.Id == id);
+            var foods = context.Food.Where(x => x.Status).ToList();
+            return View("Menu", foods);
         }
 
         public IActionResult AddModal(int id)
         {
-            return PartialView();
+            var food = context.Food.FirstOrDefault(x => x.Id == id);
+            if (food == null)
+                return NotFound();
+            return PartialView(food);
         }
 
         public IActionResult PaymentModal()
--- Restaurent.Manager/wwwroot/js/site.js
+++ Restaurent.Manager/wwwroot/js/site.js
@@ -1,11 +1,26 @@
 ﻿$(document).on('click', '.dish-option', function (e) {
     e.preventDefault();
+    var id = $(this).data('id');
     $.ajax({
-        url: '/Home/AddModal/1'
+        url: '/Home/AddModal/' + id
     }).done((res) => {
+        $('#modal').find('.modal-dialog').addClass('modal-lg');
         $('#modal-container').html(res);
         $('#modal').modal('show');
     })
+});
+
+$(document).on('click', '.options', function (e) {
+    e.preventDefault();
+    var classFor = $(this).data('for');
+    $('.options').removeClass('active');
+    $(this).addClass('active');
+
+    $('.table-view').addClass('d-none');
+    if (classFor != 'all')
+        $('.table-view.' + classFor).removeClass('d-none');
+    else
+        $('.table-view').removeClass('d-none');
 });
 
 $(document).on('click', '.btn-add-modal', function (e) {
@@ -31,6 +46,27 @@ $(document).on('click', '.payment-button', function (e) {
     });
 });
 
+$(document).on('click', '.quantity-btn', function (e) {
+    e.preventDefault();
+    var focus = $(this).data('focus');
+    if (focus == '' || focus == undefined)
+        return;
+    var value = parseInt($(focus).html());
+    if ($(this).hasClass('bonus'))
+        $(focus).html(value + 1);
+
+    if ($(this).hasClass('minus')) {
+        if (value - 1 <= 0)
+            alert("Quantity must be greater than 0");
+        else
+            $(focus).html(value - 1);
+    }
+});
+
+$(document).on('change', '.search-val', function () {
+    $(this).parents('form').submit();
+});
+
 $(document).on('click', '.role-button', function (e) {
     e.preventDefault();
     $('.role-button').removeClass('active');
@@ -38,5 +74,6 @@ $(document).on('click', '.role-button', function (e) {
 });
 
 $('#modal').on('hidden.bs.modal', (e) => {
+    $('#modal').find('.modal-dialog').removeClass('modal-lg').removeClass('modal-xl').removeClass('.modal-md');
     $('#modal-container').html('');
 })
